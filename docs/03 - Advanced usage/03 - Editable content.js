da0088dbe21c0cf9849435924ce7976c30c5517d
Biing-yann Hsiung (bear) <bhsiung@linkedin.com>
2021-09-30 00:47:41 -0700

    fix lint error

--- docs/03 - Advanced usage/03 - Editable content.js
+++ docs/03 - Advanced usage/03 - Editable content.js
@@ -1,7 +1,6 @@
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
-import { later } from '@ember/runloop';
 
 export default class SampleComponent extends Component {
   @tracked doc = '## Hello world';
